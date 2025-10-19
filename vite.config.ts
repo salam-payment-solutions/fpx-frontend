4b14ba5e70996c61d4ab387f6388c7f58bf97eee
Rahman Ghaffar <rahman.ghaffar98@gmail.com>
2025-10-19 22:38:26 +0800

    fix deployment issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig, loadEnv, type PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 import svgLoader from 'vite-svg-loader'
 import path from 'node:path'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(async ({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
     const plugins: PluginOption[] = [vue(), vueJsx(), tailwindcss(), svgLoader()]
 
     // Only add DevTools in development mode
     if (mode === 'development') {
+        const { default: vueDevTools } = await import('vite-plugin-vue-devtools')
         plugins.push(vueDevTools())
     }
 
